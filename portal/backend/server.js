520e571640cb4e429a11f814cebe2cd91823e090
Shubh-13 <mail.shubhankarsingh@gmail.com>
2022-05-28 00:34:50 +0530

    removing commented code

--- portal/backend/server.js
+++ portal/backend/server.js
@@ -8,26 +8,6 @@ app.use(cors({
   origin: "*"
 }));
 
-// app.get('/', (req, res) => {
-
-//   console.log(req);
-
-//   res.setHeader('Content-type', 'application/zip');
-
-//   let fileStream = fs.createReadStream("./templates/express-react.zip");
-//   fileStream.pipe(res);
-
-//   fileStream.on('error', function (error) {
-//     console.log('filestream error');
-//   });
-
-//   fileStream.on('close', function () {
-//     /** Close Filestream  **/
-//     console.log('filestream success');
-//     fileStream.destroy();
-//   });
-// })
-
 app.get('/download/:frontend/:backend', (req, res) => {
 
   let frontend = req.params.frontend;
